5efa286644d8711ffbb92af7a5daadb5e69b9940
marc <marc@mhou.es>
2022-04-25 20:54:12 +0200

    proyecto inicial

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+	content: [
+		'./index.html',
+	],
+	theme: {
+		screens: {
+			sm: '28rem',
+			md: '48rem',
+			lg: '64rem',
+			xl: '80rem',
+		},
+		container: {
+			center: true,
+			padding: {
+				DEFAULT: '1rem',
+				sm: '2rem',
+				xl: '4rem',
+			},
+		},
+		fontFamily: {
+			sans: 'Futura, sans-serif',
+			mono: 'monospace',
+			serif: 'serif',
+		},
+	},
+}
